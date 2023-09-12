d92e89306ec504854e6dc66fcf68dd72620e9a30
Carlos Sánchez <oceanrdn@gmail.com>
2023-09-12 18:08:16 +0200

    Read contract with params

--- scripts/4_readContract.ts
+++ scripts/4_readContract.ts
@@ -2,10 +2,22 @@ import { mainnetDAI } from "../contracts/mainnetDAI";
 import { mainnetClient } from "./1_client";
 import { formatEther } from "viem";
 
-const data = await mainnetClient.readContract({
+const totalSupply = await mainnetClient.readContract({
   address: mainnetDAI.address,
   abi: mainnetDAI.abi,
   functionName: "totalSupply",
 });
 
-console.log("The max supply of DAI is", formatEther(data));
+console.log("The max supply of DAI is", formatEther(totalSupply));
+
+const austinAddress = await mainnetClient.getEnsAddress({ name: "atg.eth" });
+if (austinAddress) {
+  const data = await mainnetClient.readContract({
+    address: mainnetDAI.address,
+    abi: mainnetDAI.abi,
+    functionName: "balanceOf",
+    args: [austinAddress],
+  });
+
+  console.log("Austin's DAI balance", formatEther(data));
+}
